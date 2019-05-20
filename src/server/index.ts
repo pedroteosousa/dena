e15c4df1bd1410352b2e65333464d5c07055212a
Pedro Sousa <pedroteosousa@gmail.com>
2019-05-20 16:30:36 -0300

    Add auth route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,12 @@
 import Hapi from '@hapi/hapi'
 import hapi_inert_pluggin from '@hapi/inert'
+import { OAuth2Client } from 'google-auth-library'
 import path from 'path'
 
 import dotenv from 'dotenv'
 dotenv.config()
+
+import auth_route from './auth'
 
 const init = async () => {
   const server = new Hapi.Server({
@@ -13,17 +16,20 @@ const init = async () => {
 
   await server.register(hapi_inert_pluggin)
 
-  server.route({
-    handler: {
-      directory: {
-        index: true,
-        listing: true,
-        path: path.join(__dirname, '../../lib/client'),
+  server.route([
+    {
+      handler: {
+        directory: {
+          index: true,
+          listing: true,
+          path: path.join(__dirname, '../../lib/client'),
+        },
       },
+      method: 'GET',
+      path: '/{path*}',
     },
-    method: 'GET',
-    path: '/{path*}',
-  })
+    auth_route,
+  ])
 
   await server.start()
   console.log(`Server running on ${server.info.uri}`)
