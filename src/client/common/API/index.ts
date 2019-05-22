a93728117105cde201b8aedbda51ad439d519577
Pedro Sousa <pedroteosousa@gmail.com>
2019-05-22 01:54:21 -0300

    Use URL env variable as server origin

--- src/client/common/API/index.ts
+++ src/client/common/API/index.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-const PORT = process.env.PORT
-const URL = process.env.URL + (PORT ? `:${PORT}` : '')
+const URL = process.env.URL
 
 interface AuthResponse {
   user: string
